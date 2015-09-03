5ec65668b13a3610ba17157da5ed2b11b8463b16
huongluu <huong84a1@gmail.com>
2015-09-03 18:13:19 -0500

    “wip”

--- protected/views/charts/scatter.js.php
+++ protected/views/charts/scatter.js.php
@@ -93,7 +93,17 @@ $series4_str = rtrim($series4_str, ",");
                     name: '<?php echo $chart["series"][0]["series2-name"] ?>',
                     color: 'rgba(119, 152, 191, .5)',
                     data: [<?php echo $series2_str; ?>]
-                }]
+                }, {
+                    name: '<?php echo $chart["series"][0]["series3-name"] ?>',
+                    color: 'rgba(119, 152, 191, .5)',
+                    data: [<?php echo $series2_str; ?>]
+                }, {
+                    name: '<?php echo $chart["series"][0]["series4-name"] ?>',
+                    color: 'rgba(119, 152, 191, .5)',
+                    data: [<?php echo $series2_str; ?>]
+                }
+                
+                ]
         });
     });
 </script>
