9e5e5654ee490add129d082e43713ab5ecc0505e
huongluu <huong84a1@gmail.com>
2015-09-09 17:16:37 -0500

    Keep the boxplot for now, Get boxplot and Table data by 1 query

--- protected/views/jobs/_chart_table.php
+++ protected/views/jobs/_chart_table.php
@@ -4,7 +4,7 @@ include_once 'utils.php';
 $id = $_GET['c'];
 $chart = getChartInfo($id);
 
-if ($chart["type"] == "table") {
+if ($chart["type"] == "boxplot") {
     include_once dirname(__FILE__) . '/../tables/topk.tb.php';
 }
 //print_r($chart);
