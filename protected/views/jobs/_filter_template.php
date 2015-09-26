d083220af06d3e9cdc822ce55a2b3e3b7ca25093
Aleyasen <amir.8829@gmail.com>
2015-09-26 17:11:50 -0500

    reorganize/rename the configuration files for previous charts

--- protected/views/jobs/_filter_template.php
+++ protected/views/jobs/_filter_template.php
@@ -3,8 +3,10 @@
 include_once 'utils.php';
 $id = $_GET['c'];
 $chart = getChartInfo($id);
-$filter = $chart['filter'];
-$filter_path = __DIR__ . '/../' . $filter;
-include_once $filter_path;
+if (isset($chart['filter'])) {
+    $filter = $chart['filter'];
+    $filter_path = __DIR__ . '/../' . $filter;
+    include_once $filter_path;
+}
 ?>
 
