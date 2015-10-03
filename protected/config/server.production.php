4e6fecf627675d47f11f71cf459f38fb1076ac21
Aleyasen <amir.8829@gmail.com>
2015-10-03 09:46:46 -0500

    update .gitignore

---
+++ protected/config/server.production.php
@@ -0,0 +1,20 @@
+<?php
+// WARNING: DON'T USE THIS CONFIGURATION FILE. PLEASE USE server.production.php INSTEAD.
+// 
+// uncomment the following to define a path alias
+// Yii::setPathOfAlias('local','path/to/local-folder');
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+return array(
+    // application components
+    'components' => array(
+        'db' => array(
+            'connectionString' => 'mysql:host=172.22.165.28;dbname=mira_final',
+            'username' => '',
+            'password' => '',
+        // Enable profiling
+        // 'enableProfiling' => true,
+        // 'enableParamLogging' => true,
+        ),
+    ),
+);
