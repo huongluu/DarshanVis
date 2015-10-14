7bb70cca97856214b783f30e8016bf0bc838bc2d
Aleyasen <amir.8829@gmail.com>
2015-10-14 09:14:00 -0500

    move chart tooltip to json files

--- protected/views/charts/bar.scatter.js.php
+++ protected/views/charts/bar.scatter.js.php
@@ -4,34 +4,6 @@ include_once 'utils2.php';
 <script type="text/javascript">
 
 $(function () {
-  $('#tooltip1').tooltip({
-    title:
-    'Non-global data I/O: The amount of time this job spent in function calls to read/write its files not accessed by all processes.'
-  });
-  $('#tooltip2').tooltip({
-    title:
-    'Non-global Metadata: The amount of time this job spent in metadata function calls (open, close, seek, etc.) for non-global files, i.e., files that one or more but not all processes opened.'
-  });
-  $('#tooltip3').tooltip({
-    title:
-    'Global data I/O: The amount of time this job spent in function calls to read/write global files, i.e., files that all processes opened.'
-  });
-  $('#tooltip4').tooltip({
-    title:
-    'Global Metadata: The amount of time this job spent in metadata function calls (open, close, seek, etc.) for global files, i.e., files that all processes opened.'
-  });
-  $('#tooltip5').tooltip({
-    title:
-    'Not I/O: The amount of time this job spent outside of I/O function calls (data and metadata).'
-  });
-  $('#tooltip6').tooltip({
-    title:
-    '# of Processes: The number of processes this job had.'
-  });
-  $('#tooltip7').tooltip({
-    title:
-    'Total Bytes Read/Written: The total number of bytes this job read and wrote.'
-  });
   //        $("#tooltip1").tooltip('show');
   //        $('[data-toggle="tooltip"]').tooltip();
   var globalCallback = function (chart) {
@@ -109,11 +81,6 @@ $(function () {
     //                color: color ? null : Highcharts.getOptions().colors[1]
     //            });
     //            color = !color;
-
-
-
-
-
   });
 
 });
--- protected/views/jobs/_charts.php
+++ protected/views/jobs/_charts.php
@@ -9,26 +9,16 @@ include '_chart_template.php';
 
 
 <div id="chart-container"  style="height: 400px; width: 900px; margin: 0 auto;"></div>
-<div style="margin-left: 40%; position: relative; top: -60px; width: 400px;">
-    <center>
-        <span id="tooltip1"></span>
-        <span id="tooltip2"></span>
-        <span id="tooltip3"></span>
-        <span id="tooltip4"></span>
-        <span id="tooltip5"></span>
-        <span id="tooltip6"></span>
-        <span id="tooltip7"></span>
-    </center>
-</div>
+
 
 <div id="chart-config" hidden>
-  X Axis
-  <select id="chart-config-sel-x">
-  </select>
-  Y Axis
-  <select id="chart-config-sel-y">
-  </select>
-  <button id="chart-config-button" class="btn btn-medium btn-success">Configure Chart</button>
+    X Axis
+    <select id="chart-config-sel-x">
+    </select>
+    Y Axis
+    <select id="chart-config-sel-y">
+    </select>
+    <button id="chart-config-button" class="btn btn-medium btn-success">Configure Chart</button>
 </div>
 
 <?php
