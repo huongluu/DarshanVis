aae5ce8c1b7b6b43cbed21597d37f5f803a4b0e5
amir.8829@gmail.com <amir.8829@gmail.com>
2015-10-15 17:15:26 -0500

    change location of export button

--- protected/views/charts/bar.scatter.js.php
+++ protected/views/charts/bar.scatter.js.php
@@ -69,13 +69,20 @@ include_once 'utils2.php';
             chart.yAxis[0].axisTitle.attr({
                 text: stacking ? "Distribution of time (s)" : "Percentage of time (%)"
             });
-//            if (!stacking) {
-//                chart.yAxis[0].setExtremes(0, 100);
-//                console.log("set min and max to 0 and 100");
-//            } else {
+            if (!stacking) {
+                chart.yAxis[0].setExtremes(0, 100);
+               
+                console.log(chart.yAxis[0]);
+                console.log("set min and max to 0 and 100");
+            } else {
 //                chart.yAxis[0].setExtremes(null, null);
-//                console.log("set min and max to null and null");
-//            }
+                chart.yAxis[0].update({
+                    max: null,
+                    min: null
+                });
+                console.log(chart.yAxis[0]);
+                console.log("set min and max to null and null");
+            }
             stacking = !stacking;
             //            chart.series[0].update({
             //                color: color ? null : Highcharts.getOptions().colors[1]
