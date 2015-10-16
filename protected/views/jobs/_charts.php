4bb56f5f14a8f15e75c3795926ded26491be000d
Yasha Mostofi <yasha.mostofi@gmail.com>
2015-10-15 22:16:16 -0500

    grid

--- protected/views/jobs/_charts.php
+++ protected/views/jobs/_charts.php
@@ -23,21 +23,21 @@ include '_chart_template.php';
 
 <div id="15-scatter-containers" hidden>
   <div class="row">
-    <div id="chart-container-1" class="col-md-4 charts" style="height: 400px; width: 900px; margin: 0 auto;"></div>
-    <div id="chart-container-2" class="col-md-4 charts"  style="height: 400px; width: 900px; margin: 0 auto;"></div>
-    <div id="chart-container-3" class="col-md-4 charts"  style="height: 400px; width: 900px; margin: 0 auto;"></div>
-    <div id="chart-container-4" class="col-md-4 charts"  style="height: 400px; width: 900px; margin: 0 auto;"></div>
-    <div id="chart-container-5" class="col-md-4 charts"  style="height: 400px; width: 900px; margin: 0 auto;"></div>
-    <div id="chart-container-6" class="col-md-4 charts"  style="height: 400px; width: 900px; margin: 0 auto;"></div>
-    <div id="chart-container-7" class="col-md-4 charts"  style="height: 400px; width: 900px; margin: 0 auto;"></div>
-    <div id="chart-container-8" class="col-md-4 charts"  style="height: 400px; width: 900px; margin: 0 auto;"></div>
-    <div id="chart-container-9" class="col-md-4 charts"  style="height: 400px; width: 900px; margin: 0 auto;"></div>
-    <div id="chart-container-10" class="col-md-4 charts"  style="height: 400px; width: 900px; margin: 0 auto;"></div>
-    <div id="chart-container-11" class="col-md-4 charts"  style="height: 400px; width: 900px; margin: 0 auto;"></div>
-    <div id="chart-container-12" class="col-md-4 charts"  style="height: 400px; width: 900px; margin: 0 auto;"></div>
-    <div id="chart-container-13" class="col-md-4 charts"  style="height: 400px; width: 900px; margin: 0 auto;"></div>
-    <div id="chart-container-14" class="col-md-4 charts"  style="height: 400px; width: 900px; margin: 0 auto;"></div>
-    <div id="chart-container-15" class="col-md-4 charts"  style="height: 400px; width: 900px; margin: 0 auto;"></div>
+    <div id="chart-container-1" class="col-md-4 charts" style="height: 400px; margin: 0 auto;"></div>
+    <div id="chart-container-2" class="col-md-4 charts"  style="height: 400px; margin: 0 auto;"></div>
+    <div id="chart-container-3" class="col-md-4 charts"  style="height: 400px; margin: 0 auto;"></div>
+    <div id="chart-container-4" class="col-md-4 charts"  style="height: 400px; margin: 0 auto;"></div>
+    <div id="chart-container-5" class="col-md-4 charts"  style="height: 400px; margin: 0 auto;"></div>
+    <div id="chart-container-6" class="col-md-4 charts"  style="height: 400px; margin: 0 auto;"></div>
+    <div id="chart-container-7" class="col-md-4 charts"  style="height: 400px; margin: 0 auto;"></div>
+    <div id="chart-container-8" class="col-md-4 charts"  style="height: 400px; margin: 0 auto;"></div>
+    <div id="chart-container-9" class="col-md-4 charts"  style="height: 400px; margin: 0 auto;"></div>
+    <div id="chart-container-10" class="col-md-4 charts"  style="height: 400px; margin: 0 auto;"></div>
+    <div id="chart-container-11" class="col-md-4 charts"  style="height: 400px; margin: 0 auto;"></div>
+    <div id="chart-container-12" class="col-md-4 charts"  style="height: 400px; margin: 0 auto;"></div>
+    <div id="chart-container-13" class="col-md-4 charts"  style="height: 400px; margin: 0 auto;"></div>
+    <div id="chart-container-14" class="col-md-4 charts"  style="height: 400px; margin: 0 auto;"></div>
+    <div id="chart-container-15" class="col-md-4 charts"  style="height: 400px; margin: 0 auto;"></div>
   </div>
 </div>
 
