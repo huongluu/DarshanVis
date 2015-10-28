fce0c31adef5ce19f3c12cf71c59a908c0a3a469
amir.8829@gmail.com <amir.8829@gmail.com>
2015-10-28 01:53:24 -0500

    fixing filtering for platform and apps sections

---
+++ protected/views/components/_filters_for_platform_top.php
@@ -0,0 +1,212 @@
+<?php
+$display_en = false;
+?>
+<script type="text/javascript">
+    $(function () {
+
+        function send() {
+            var datepickerobj = $('#reportrange').data('daterangepicker');
+            var filter = {
+                numapp: $("#numapp-textbox").val(),
+                application: $("#application-textbox").val(),
+                user: $("#user-textbox").val(),
+                sort_level1: $("#sort-level1 option:selected").val(),
+                mode_level1: $("#mode-level1 option:selected").val(),
+                sort_level2: $("#sort-level2 option:selected").val(),
+                mode_level2: $("#mode-level2 option:selected").val(),
+                sort_level3: $("#sort-level3 option:selected").val(),
+                mode_level3: $("#mode-level3 option:selected").val(),
+                start_date: datepickerobj.startDate.format('YYYY-MM-DD'),
+                end_date: datepickerobj.endDate.format('YYYY-MM-DD'),
+                url: window.location.href
+            }
+
+            $('#status').html('filtering..');
+            $url = 'filter';
+//            alert($url);
+            $.ajax({
+                url: $url,
+                type: 'post',
+                dataType: 'json',
+                success: function (data) {
+//                    alert("success");
+                    console.log(data);
+                    var chart = $('#chart-container').highcharts();
+                    while (chart.series.length > 0) {
+                        chart.series[0].remove(false);
+                    }
+                    var series = data["chart"]["series"];
+                    var queryResult = data["queryresult"];
+                    for (var i = 0; i < series.length; i++) {
+
+                        var attr = series[i]["attribute"];
+                        var qr = queryResult[attr];
+                        if (qr != null) {
+                            $('#chart-container').html("");
+                            series[i]["data"] = [];
+                            for (var j = 0; j < qr.length; j++) {
+                                series[i]["data"].push([j, Number(qr[j])]);
+                            }
+                            console.log(series[i]);
+                            chart.addSeries(series[i], false);
+                        } else {
+                            $('#chart-container').html("<center>No result for the desired filters.</center>");
+                        }
+                    }
+//                    series.forEach(function (s) {
+//                        chart.addSeries(s, false);
+//                    });
+
+                    chart.redraw();
+                    $('#chart-container').css('visibility', 'visible');
+                    $('#chart-container').css('display', 'block');
+                    $('#status').html('&nbsp;');
+//                    $('#target').html(data);
+                },
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    console.log(XMLHttpRequest);
+                    console.log(textStatus);
+                    console.log(errorThrown);
+                    $('#status').html('error!');
+                },
+                data: filter
+            });
+        }
+
+
+        $("#filter-button, #sorting-button").click(function () {
+            send();
+        });
+
+        $('#reportrange span').html(moment().subtract(1, 'years').format('MMM DD \'YY') + ' - ' + moment().format('MMM DD \'YY'));
+        $('#reportrange').daterangepicker({
+            format: 'MM/DD/YYYY',
+            startDate: moment().subtract(1, 'years'),
+            endDate: moment(),
+            minDate: '01/01/2012',
+            maxDate: '12/31/2015',
+//            dateLimit: {
+//                days: 60
+//            },
+            showDropdowns: true,
+            showWeekNumbers: true,
+            timePicker: false,
+            timePickerIncrement: 1,
+            timePicker12Hour: true,
+            ranges: {
+                'Today': [moment(), moment()],
+                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            },
+            opens: 'left',
+            drops: 'down',
+            buttonClasses: ['btn', 'btn-sm'],
+            applyClass: 'btn-primary',
+            cancelClass: 'btn-default',
+            separator: ' to ',
+            locale: {
+                applyLabel: 'Submit',
+                cancelLabel: 'Cancel',
+                fromLabel: 'From',
+                toLabel: 'To',
+                customRangeLabel: 'Custom',
+                daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+                monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                firstDay: 1
+            }
+        },
+        function (start, end, label) {
+            console.log(start.toISOString(), end.toISOString(), label);
+            $('#reportrange span').html(start.format('MMM DD \'YY') + ' - ' + end.format('MMM DD \'YY'));
+        });
+
+        $('#user-textbox').typeahead({
+            source: function (query, process) {
+                return $.get('UserList', {
+                    user: query,
+                    application: $("#application-textbox").val() ? $("#application-textbox").val() : "null"
+                },
+                function (data) {
+                    console.log(data);
+                    return process(data);
+                });
+            }
+        });
+
+        $('#application-textbox').typeahead({
+            source: function (query, process) {
+                return $.get('ApplicationList', {
+                    application: query,
+                    user: $("#user-textbox").val() ? $("#user-textbox").val() : "null"
+                },
+                function (data) {
+                    console.log(data);
+                    return process(data);
+                });
+            }
+        });
+
+    });
+</script>
+
+<div>
+    <div class="row">
+        <div class="text-center" id="status">&nbsp;</div>
+    </div>
+    <div class="row">
+        <div class="form-group col-md-4">
+            <div class="input-group">
+                <span class="input-group-addon">
+                    <i class="glyphicon glyphicon-user" data-toggle="tooltip" data-placement="left" title="My Tooltip text"></i>
+                </span>
+                <input type="text" id="numapp-textbox" name="numapp" class="form-control" id="numapp-typeahead" data-provide="typeahead" placeholder="Number of Applications" autocomplete="off">
+            </div>
+        </div>
+
+        <div class="form-group col-md-2">
+            <div class="input-group">
+                <span class="input-group-addon">
+                    <i class="glyphicon glyphicon-user" data-toggle="tooltip" data-placement="left" title="My Tooltip text"></i>
+                </span>
+                <input type="text" id="user-textbox" name="user" class="form-control" id="user-typeahead" data-provide="typeahead" placeholder="UserID" autocomplete="off">
+            </div>
+        </div>
+
+        <div class="form-group col-md-3">
+            <div id="reportrange" class="form-control" style="background: #fff; cursor: pointer; padding: 0px 5px; border: 1px solid #ccc">
+                <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
+                <span></span> <b class="caret"></b>
+            </div>
+        </div>
+
+        <div class="col-md-1 form-group">
+            <button id="filter-button" class="btn btn-inverse tiny-button">Update</button>
+        </div>
+
+        <div class="col-md-1 form-group">
+            <button type="button" class="btn tiny-button" data-toggle="modal" href="#sorting_modal">
+                <i class="glyphicon glyphicon-sort-by-alphabet"></i>
+                Sort</button>
+        </div>
+    </div>
+
+    <!--
+        <div class="row">
+    
+        </div>-->
+
+    <?php include '_sorting_modal.php'; ?>
+</div>
+
+<div class="row">
+    <div class="col-md-2 form-group">
+        <button style="font-size: 150%;" type="button" id="toggle-percentage" class="btn tiny-button">
+            %</button>
+    </div>
+</div>
+
+
+
