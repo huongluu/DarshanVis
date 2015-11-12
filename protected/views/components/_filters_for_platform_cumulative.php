fdc8f91e41f2cadf8fc7b08952ba379e9721c2fd
huongluu <huong84a1@gmail.com>
2015-11-12 14:38:07 -0600

    Remove App Name and UserID filter in Cumulative Usage of I/O time - Still need to implement Time filter

---
+++ protected/views/components/_filters_for_platform_cumulative.php
@@ -0,0 +1,37 @@
+<?php
+$display_en = false;
+?>
+
+<div>
+    <div class="row">
+        <div class="text-center" id="status">&nbsp;</div>
+    </div>
+    <div class="row">
+
+        <div class="form-group col-md-3">
+            <div id="reportrange" class="form-control" style="background: #fff; cursor: pointer; padding: 0px 5px; border: 1px solid #ccc">
+                <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
+                <span></span> <b class="caret"></b>
+            </div>
+        </div>
+
+        <div class="col-md-1 form-group">
+            <button id="filter-button" class="btn btn-inverse tiny-button">Update</button>
+        </div>
+
+    </div>
+
+    <!--
+        <div class="row">
+
+        </div>-->
+
+    <?php include '_sorting_modal.php'; ?>
+</div>
+
+<div class="row">
+    <div class="col-md-2 form-group">
+        <button style="font-size: 150%;" type="button" id="toggle-percentage" class="btn tiny-button">
+            %</button>
+    </div>
+</div>
